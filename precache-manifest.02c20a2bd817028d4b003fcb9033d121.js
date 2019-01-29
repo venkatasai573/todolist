19cddea404e99de1a5535dbd9e75b076f2705122
saichinnu <venkatasai.573@gmail.com>
2019-01-29 23:00:40 +0530

    Updates

---
+++ precache-manifest.02c20a2bd817028d4b003fcb9033d121.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "bd979691ba11f10f0ebc",
+    "url": "/todolist/static/js/main.bd979691.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/todolist/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "bd979691ba11f10f0ebc",
+    "url": "/todolist/static/css/main.a52ba96b.chunk.css"
+  },
+  {
+    "revision": "ff81d710c9d1bd7c5a455f4a2fef1ff5",
+    "url": "/todolist/index.html"
+  }
+];
